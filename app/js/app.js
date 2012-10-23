15e771a356ce612ffd36bc240f709b3c0527136e
Arjun Sanyal <arjun.sanyal@childrens.harvard.edu>
2012-10-23 16:03:09 -0400

    Configure ng to use {[{ }]} for interpolation

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,17 @@
 'use strict';
 
 // simplified module definition: no filters, services, or directives
-// and only one view
+// and only one view without a parital. It's also configured with
+// alternate interpolation delimiters as to not conflict with Django
 angular.module('myApp', [])
   .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
+    $routeProvider.when('/view1')
     $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+  }])
+  .config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[{');
+    $interpolateProvider.endSymbol('}]}');
+  });
 
 // Declare app level module which depends on filters, and services
 //angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
